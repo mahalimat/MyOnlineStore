e122b44cefe8e30c1b2d1499d4f329bcb76768be
mahalimat <mathanm@gmail.com>
2019-12-13 08:18:55 -0500

    Added checkout items code

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,14 @@
-import React from "react";
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-import SHOP_Data from "./shop.data.js";
-import CollectionPreview from "../../components/collection-preview/collection-preview";
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_Data
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionsprops }) => (
-          <CollectionPreview key={id} {...otherCollectionsprops} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
 export default ShopPage;
