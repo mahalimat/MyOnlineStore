a771e192c1e19140d2e9c51a0e948e356a27e523
mahalimat <mathanm@gmail.com>
2019-12-14 18:58:04 -0500

    moved the shop data to fire store

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,16 +1,16 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyDTlJPgk0r3jgmV8cfwgN_aNxMj-JT1-ho",
-  authDomain: "crwn-db-ee600.firebaseapp.com",
-  databaseURL: "https://crwn-db-ee600.firebaseio.com",
-  projectId: "crwn-db-ee600",
-  storageBucket: "crwn-db-ee600.appspot.com",
-  messagingSenderId: "955564785384",
-  appId: "1:955564785384:web:94d0589863c0dda77d135d",
-  measurementId: "G-JFVEEP800E"
+  apiKey: 'AIzaSyDTlJPgk0r3jgmV8cfwgN_aNxMj-JT1-ho',
+  authDomain: 'crwn-db-ee600.firebaseapp.com',
+  databaseURL: 'https://crwn-db-ee600.firebaseio.com',
+  projectId: 'crwn-db-ee600',
+  storageBucket: 'crwn-db-ee600.appspot.com',
+  messagingSenderId: '955564785384',
+  appId: '1:955564785384:web:94d0589863c0dda77d135d',
+  measurementId: 'G-JFVEEP800E'
 };
 
 firebase.initializeApp(config);
@@ -38,15 +38,46 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
 
   return userRef;
-}
+};
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
 
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
